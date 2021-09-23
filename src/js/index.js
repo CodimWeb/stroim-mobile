d36e64ede18f1956cbf16612bfe9733c23fa7436
Виталий Шлепаков <shlepakov58@gmail.com>
2021-09-23 21:39:30 +0200

    uikit

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,14 @@ import $ from 'jquery';
 
 import Util from 'bootstrap/js/dist/util.js'
 import Modal from 'bootstrap/js/dist/modal'
-// import 'slick-carousel';
 import slick from 'slick-carousel';
+import moment from 'moment';
+import daterangepicker from 'jquery-date-range-picker'
 
+
+//styles
+import "jquery-date-range-picker/dist/daterangepicker.min.css"
+import 'slick-carousel';
 import '../scss/style.scss';
 
 $(document).ready(function(){
@@ -36,4 +41,59 @@ $(document).ready(function(){
         nextArrow: $('.review-slider-next'),
         dots: false
     });
-})
+
+    $(document).on('focus', '.materil-group__input', function() {
+        $(this).closest('.materil-group').addClass('active');
+    })
+
+    $(document).on('blur', '.materil-group__input', function(e) {
+        if (e.target.value == '') {
+            $(this).closest('.materil-group').removeClass('active');
+        }
+    })
+
+    $('.materil-group__input').each((index, item ) => {
+        if($(item).val()) {
+            $(item).closest('.materil-group').addClass('active');
+        }
+    })
+
+
+    initialDatePicker()
+});
+
+function initialDatePicker() {
+
+    /*Tender open time range*/
+    moment.locale("ru");
+    if(document.querySelector('input[name="daterange"]')) {
+        $('input[name="daterange"]').dateRangePicker({
+            separator: ' по ',
+            language: 'ru',
+            startOfWeek: 'monday',
+            startDate: Date.now(),
+            format: 'HH:mm DD MMMM YYYY',
+            time: {
+                enabled: true
+            },
+            /*defaultTime: moment().startOf('day').hour(9).minute(0).toDate(),*/
+            defaultEndTime: moment().startOf('day').hour(17).minute(0).toDate(),
+            minDays: 1,
+            maxDays: 15,
+            selectForward: true,
+            autoClose: true,
+            setValue: function (s, s1, s2) {
+                $('input[name="daterange"]').val(s);
+            },
+            customOpenAnimation: function (cb) {
+                $(this).fadeIn(300, cb);
+            },
+            customCloseAnimation: function (cb) {
+                $(this).fadeOut(300, cb);
+            }
+        }).bind('datepicker-change',function(event,obj) {
+            $('#startDate').val(obj.date1.getTime());
+            $('#expiryDate').val(obj.date2.getTime());
+        });
+    }
+};
